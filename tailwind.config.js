4738c3a32256eb437eab8fa942e80ca7e424badb
Juseong (Joe) Kim <juseong.kim@hotmail.com>
2020-11-28 20:08:54 +0900

    set up tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily:{
+      sans: ['Nunito','sans-serif'],
+      display: ['Nunito','sans-serif'],
+      body: ['Nunito','sans-serif']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer')
+  ],
+}
+
+// npx tailwindcss build main.css -o output.css
